624f420e8d0aa8c953d7d420714873be93b54284
gowthamRS009 <rsgowtham218@gmail.com>
2025-01-06 11:11:51 +0530

    First

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic.Constructor
+{
+    //Constructors (Every Constructor As Implicit Const)
+    // constructor is a special method used to initialize objects of a class.
+    //It is called automatically when an instance of the class is created.
+    // It Class Name and Method name Exactly same
+    //cannot have any returntype
+    // And its Non-value returning method
+    // implicit Defined constructor are public 
+    //Explicit Constuctor Are Parameterless const or Parameterized Constructor
+    //multiple const defined and using different parameter type
+
+    //Default Constructor:Parameteless Constructor are also known As Default Constructor.
+    //(Every parameterless const will initialize of variable in the class with default values)
+
+    //Default or Parameterless Constructor: if a constructor mrthod doesnot take any parameters
+    //Implicit(compiler create) Constructor are Parameterless Constructor
+
+    //Parameterized Constructor: Explicit Constuctor
+   
+    //Static Constructor:  
+    //Private Constructor:
+    //Copy Constructor
+
+
+    //internal class Program
+    //{
+    //this class Contains implicit Constructor initialize value of int,bool
+    //class ProgramClass
+    //{
+    //    int i; bool b;
+    //    static void Main(string[] args)
+    //    {
+    //        ProgramClass p =new ProgramClass();
+    //        Console.WriteLine(p.i);
+    //        Console.WriteLine(p.b);
+    //    }
+    //}
+    //-------------------------------------
+
+}      
